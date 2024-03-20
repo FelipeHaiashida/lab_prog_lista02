c2866001b0ddcf5288413bddafe1f2b99095b9ab
FelipeHaiashida <112267078+FelipeHaiashida@users.noreply.github.com>
2024-03-20 17:42:13 -0300

    Update q1.c

--- q1.c
+++ q1.c
@@ -15,19 +15,19 @@ int main() {
                     printf("Domingo\n");
                     break;
                 case 2:
-                    printf("Segunda-feira\n");
+                    printf("Segunda feira\n");
                     break;
                 case 3:
-                    printf("Terça-feira\n");
+                    printf("Terça feira\n");
                     break;
                 case 4:
-                    printf("Quarta-feira\n");
+                    printf("Quarta feira\n");
                     break;
                 case 5:
-                    printf("Quinta-feira\n");
+                    printf("Quinta feira\n");
                     break;
                 case 6:
-                    printf("Sexta-feira\n");
+                    printf("Sexta feira\n");
                     break;
                 case 7:
                     printf("Sábado\n");
